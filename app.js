90c02e5e590698ff5f2336b7476c3d6572fd9159
jonhewitt <jchewitt.jh@gmail.com>
2018-10-19 15:35:56 -0500

    added test

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const db = require('./db');
 global.__root   = __dirname + '/';
 const VerifyToken = require(__root + 'auth/verify-token');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api', function (req, res) {
     res.status(200).send('API works.');
 });
